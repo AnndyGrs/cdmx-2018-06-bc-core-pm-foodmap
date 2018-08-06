ba3a55e865f4b3235948575de7187308b81f7283
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-08-06 18:08:43 -0500

    Se establecio el tiempo de animacion, y la redireccion a home.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,8 @@
 const splash = () => {
-  let preload = document.getElementById('preload');
-  let loading = 0;
-  let id = setInterval(frame, 64);
-  const frame = () => {
-    if (loading === 100) {
-      console.log('Hoola');
-      clearInterval(id);
-      window.href('views/home.html', '_self');
-    } else {
-      loading = loading++;
-      if (loading === 90) {
-        preload.style.animation = 'fadeout 1s ease';
-      }
-    }
-  };
+  setInterval(function() {
+    console.log('Hoola');
+    location.assign('views/home.html');
+  }, 4000);
 };
 
 splash();
