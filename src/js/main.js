0a038add4415f69eaae05a01a2b85ed508cb6801
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-08-06 13:56:11 -0500

    Trabajando en la funcionalidad del cambio de pagina

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,19 @@
-window.onload = timeout;
-function timeout() {
-    window.setTimeout("redirect()", 3500)
-}
+const splash = () => {
+  let preload = document.getElementById('preload');
+  let loading = 0;
+  let id = setInterval(frame, 64);
+  const frame = () => {
+    if (loading === 100) {
+      console.log('Hoola');
+      clearInterval(id);
+      window.href('views/home.html', '_self');
+    } else {
+      loading = loading++;
+      if (loading === 90) {
+        preload.style.animation = 'fadeout 1s ease';
+      }
+    }
+  };
+};
 
-function redirect() {
-    window.location = "index.html"
-    return
-}
+splash();
