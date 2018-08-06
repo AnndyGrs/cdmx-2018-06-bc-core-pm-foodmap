8dd6b6137641d99034e3357fbbacb6389dbfc0a9
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-08-06 07:56:10 -0500

    Se agrego la inicializacion a Firebase

---
+++ src/js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+let config = {
+    apiKey: 'AIzaSyDXv2qOdI9TDhoy7oEQuFPIwpfYw-qBG5E',
+    authDomain: 'foodmap-e8224.firebaseapp.com',
+    databaseURL: 'https://foodmap-e8224.firebaseio.com',
+    projectId: 'foodmap-e8224',
+    storageBucket: '',
+    messagingSenderId: '24019178294'
+  };
+  firebase.initializeApp(config);
